182eb23eebeb258681d0ee1d8bc513e0a53c3f44
Ben Kuper <bkuperberg@gmail.com>
2020-12-09 12:17:16 +0100

    organic update and windows github action

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0b15";
+    const char* const  versionString  = "1.0.0b16";
     const int          versionNumber  = 0x10000;
 }
 #endif
