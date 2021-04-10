ef4367536cfcbfa1a81471300f0fea8d20fcf476
Ben Kuper <bkuperberg@gmail.com>
2021-04-10 14:16:20 +0200

    1.0.0b33

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "LGML";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0b32";
+    const char* const  versionString  = "1.0.0b33";
     const int          versionNumber  = 0x10000;
 }
 #endif
